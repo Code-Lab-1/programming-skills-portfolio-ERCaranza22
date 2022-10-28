7e070d461ce1c9f0a94fce9f80ef49119d946665
Errol Caranza <errolrensellecaranza@gmail.com>
2022-10-28 13:32:00 +0400

    Chapter 7: Exercise 1

---
+++ Chapter 7- Functions/Exercises/Exercise 1.py
@@ -0,0 +1,8 @@
+# Exercise 1: Message
+
+def display_message():
+    """Display a message about what I'm learning."""
+    msg = "I'm learning to store code in functions."
+    print(msg)
+
+display_message()
