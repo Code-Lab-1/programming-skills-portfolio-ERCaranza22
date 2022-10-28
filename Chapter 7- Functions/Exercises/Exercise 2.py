422415c9430230d2dc580875fed1106c9685f3a7
Errol Caranza <errolrensellecaranza@gmail.com>
2022-10-28 13:32:03 +0400

    Chapter 7: Exercise 2

---
+++ Chapter 7- Functions/Exercises/Exercise 2.py
@@ -0,0 +1,7 @@
+# Exercise 2: Favorite Book
+
+def favorite_book(title):
+    """Display a message about someone's favorite book."""
+    print(f"{title} is one of my favorite manga.")
+
+favorite_book('The Abstract Wild')
