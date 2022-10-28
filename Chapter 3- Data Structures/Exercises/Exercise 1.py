e8d69dda1dcd51280936c30e9af07f6844b78543
Errol Caranza <errolrensellecaranza@gmail.com>
2022-10-28 13:30:27 +0400

    Chapter 3: Exercise 1

---
+++ Chapter 3- Data Structures/Exercises/Exercise 1.py
@@ -0,0 +1,9 @@
+# Exercise 1: Names
+
+name = ["Paige", "Andrew", "Damian", "Kimberly", "Knox", "Ryder"]
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
