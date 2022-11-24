8d8ce37518c0bba660eb3070b2c37ce207838e40
Errol Caranza <errolrensellecaranza@gmail.com>
2022-11-24 10:26:10 +0400

    Chapter 4 Exercise 10

---
+++ Chapter 4- Control Flow/Exercises/Exercise 10.py
@@ -0,0 +1,19 @@
+# Colors in Japanese
+
+color = "green"
+
+if color == "red":
+    print("aka")
+elif color == "orange":
+    print("orenji")
+elif color == "yellow":
+    print("kiiro")
+elif color == "green":
+    print("midori")
+elif color == "blue":
+    print("aoi")
+elif color == "purple":
+    print("murasakino")
+
+else:
+    print("none")
