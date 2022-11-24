e42dbea4fb4ed28e86c750fc41ddfc435487e835
Errol Caranza <errolrensellecaranza@gmail.com>
2022-11-24 10:25:57 +0400

    Chapter 3 Exercise 8

---
+++ Chapter 3- Data Structures/Exercises/Exercise 8.py
@@ -0,0 +1,9 @@
+# Movie Casting
+
+name = ["Ryan Reynolds", "Milla Jovovich", "James Franco", "Andrew Garfield", "Laurence Fishburne"]
+print("Cast:")
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
